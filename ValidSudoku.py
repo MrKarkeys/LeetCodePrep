2640e9b4f4e62ce4600e2972d490ed123aaa1634
Karthik Renuprasad <karthikrenuprasad@gmail.com>
2023-12-28 18:21:14 -0800

    valid sudoku py

---
+++ ValidSudoku.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        grid = defaultdict(set)
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == ".":
+                    continue
+                if (board[i][j] in rows[i] or
+                board[i][j] in cols[j] or
+                board[i][j] in grid[i//3, j//3]):
+                    return False
+                else:
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    grid[i//3, j//3].add(board[i][j])
+        return True
+
+        
